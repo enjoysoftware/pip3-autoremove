13bfa5eb5f6451a789694aad82a6338fbe165e2b
EnjoySoftware <47489953+enjoysoftware@users.noreply.github.com>
2020-08-21 14:27:18 +0900

    Update files

---
+++ README.md
@@ -0,0 +1,25 @@
+# pip3-autoremove
+
+
+* This repository based on https://github.com/tresni/pip-autoremove and https://github.com/invl/pip-autoremove . *
+
+Remove a package and its unused dependencies.Supports both Python2 and Python3.
+
+
+## How to install
+* How to install pip3-autoremove for Python3:
+```
+sudo pip3 install python3-pip-autoremove
+```
+
+* How to install pip3-autoremove for Python2:
+```
+sudo pip install python3-pip-autoremove
+```
+
+## Usage
+```
+pip3-autoremove packages-to-uninstall
+```
+
+To remove the globally installed package, add "sudo" before the pip3-autoremove command.
--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ import pip_autoremove
 
 
 setup(
-    name="pip-autoremove",
+    name="python3-pip-autoremove",
     version=pip_autoremove.__version__,
-    description="Remove a package and its unused dependencies",
-    long_description=open('README.rst').read(),
+    description="Remove a package and its unused dependencies(Supports Python3)",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     py_modules=["pip_autoremove"],
     license='Apache License 2.0',
-    url='https://github.com/invl/pip-autoremove',
+    url='https://github.com/enjoysoftware/pip3-autoremove',
     entry_points="""
     [console_scripts]
-    pip-autoremove = pip_autoremove:main
+    pip3-autoremove = pip_autoremove:main
     """,
     classifiers=[
         'Development Status :: 4 - Beta',
